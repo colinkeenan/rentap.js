f4de4a598770dd2b408f1a8df44f2287b753aa68
Colin Keenan <colinnkeenan@gmail.com>
2018-04-15 12:08:06 -0500

    having a lot of trouble getting user answers into database because either the database is closed, or it tries to put all the answers in before getting them. currently re-opening the database before each update, but that causes race conditions with rest. need callback or maybe just put the rest into a separate test_import.js or something

--- import_storejson/import_storejson.js
+++ import_storejson/import_storejson.js
@@ -151,36 +151,43 @@ db.serialize(function() {
   //because the rentap extension allowed user to change the header address without assigning it to a new header, many tbl.headerID will be null now
   //So, find those null's and get user to choose correct header
   
-  //first, give list of headers
+  //show records not matching headers.rowid, if any
+  var badcount = 0;
+  db.each("SELECT rowid AS id, FullName, headerID FROM tbl WHERE headerID IS NULL OR headerID NOT IN (SELECT rowid FROM headers)", function(err, row) {
+    if (!badcount) {
+      console.log("\nNAMES WITH IMPROPER headerID (shown between angle brackets <>)\n");
+    }
+    console.log(row.id + " <" + row.headerID + ">:" + row.FullName + ", ");
+    badcount++;
+  });
+
+  //give list of headers
   var header1st = 1;
   db.each("SELECT rowid AS id, * FROM headers", function(err,row) {
     if (header1st) {
-      console.log("\nLIST OF HEADERS");
+      console.log("\nLIST OF HEADERS\n");
       header1st = 0;
     }
-    console.log(row.id + " " + row.Name + ": " + row.StreetAddress + ", " + row.CityStateZip + " " + row.Title)
+    console.log(row.id + " " + row.Name + ": " + row.StreetAddress + ", " + row.CityStateZip + " " + row.Title);
   });
 
-  //then, get correct headerID for each null, storing answers in array (can't immediately put into database at same time because not syncronized)
+  //then, get correct headerID for each null (This isn't working - need to put this in a function and then the rest as a callback
   var question1st = 1;
-  var headerIDarray = [];
   var readlineSync = require('readline-sync');
-  db.each("SELECT tbl.rowid AS id, tbl.FullName, tbl.headerID, headerAddresses.StreetAddress AS addr, headerAddresses.CityStateZip AS city FROM tbl JOIN headerAddresses ON id = headerAddresses.rowid WHERE headerID IS NULL", function(err, row) {
+  db.each("SELECT tbl.rowid AS id, tbl.FullName, tbl.headerID, ha.StreetAddress, ha.CityStateZip FROM tbl JOIN headerAddresses AS ha ON id = ha.rowid WHERE headerID IS NULL", function(err, row) {
     if (question1st) {
-      console.log("\nSUPPLY headerIDs THAT COULD NOT BE DETERMINED AUTOMATICALLY");
+      console.log("\nSUPPLY " + badcount + " headerIDs THAT COULD NOT BE DETERMINED AUTOMATICALLY\n");
       question1st = 0;
     }
-    console.log(row.id + ": " + row.FullName + ", " + row.addr + ", " + row.city);
+    console.log(row.id + ": " + row.FullName + ", " + row.StreetAddress + ", " + row.CityStateZip);
     var answer = readlineSync.questionInt('Correct headerID (number)? ');
-    headerIDarray.push(answer);
+    db = new sqlite3.Database('./store.db'); //by the time answer is givin, db handle closed
+    db.serialize(function() {
+      stmt = db.prepare("UPDATE tbl SET headerID = (?)");
+      stmt.run(answer);
+      stmt.finalize();
+    }); db.close();
   });
-
-  //update tbl with answers that are in headerIDarray
-  stmt = db.prepare("UPDATE tbl SET headerID = (?) WHERE headerID IS NULL");
-  for (i = 0; i < headerIDarray.length; i++) {
-    stmt.run(headerIDarray[i]);
-  }
-  stmt.finalize();
 
   //show records still not matching headers.rowid, if any
   var bad1st = 1;
@@ -189,7 +196,7 @@ db.serialize(function() {
       console.log("\nNAMES WITH IMPROPER headerID (shown between angle brackets <>)\n");
       bad1st = 0;
     }
-    console.log(row.id + ": " + row.FullName + ", " + "<" + row.headerID + ">");
+    console.log(row.id + " <" + row.headerID + ">:" + row.FullName + ", ");
   });
 
   //show it worked by listing good names and discarded ones separately along with header names
@@ -213,6 +220,7 @@ db.serialize(function() {
   });
 
 }); //ends db.serialize(function() {
+
 
 db.close();
  
