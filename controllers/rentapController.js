c18db4c21d86587e90d665a64452a502c8e8c30a
Colin Keenan <colinnkeenan@gmail.com>
2018-04-17 14:29:12 -0500

    trying to fix routes and controllers

--- controllers/rentapController.js
+++ controllers/rentapController.js
@@ -1,19 +1,11 @@
 var rentap = require('../models/rentap');
 
-exports.show_goodaps = function(req, res) {
-  res.send('NOT IMPLEMENTED: Table of All Rental Application Data not in Trash');
+exports.show_new_ap = function(req, res) {
+  res.send('NOT IMPLEMENTED: Show New (blank) Application');
 };
 
-exports.show_trashaps = function(req, res) {
-  res.send('NOT IMPLEMENTED: Table of All Trashed Rental Application Data');
-};
-
-exports.show_goodnames = function(req, res) {
-  res.send('NOT IMPLEMENTED: Listing of All Full Names not in Trash');
-};
-
-exports.show_trashnames = function(req, res) {
-  res.send('NOT IMPLEMENTED: Listing of All Trashed Full Names');
+exports.save_new_ap = function(req, res) {
+  res.send('NOT IMPLEMENTED: Save New (filled in) Application');
 };
 
 exports.show_ap = function(req, res) {
@@ -36,12 +28,25 @@ exports.restore_ap = function(req, res) {
   res.send('NOT IMPLEMENTED: Restore Discarded Application: ' + req.params.ap_id);
 };
 
-exports.show_new_ap = function(req, res) {
-  res.send('NOT IMPLEMENTED: Show New (blank) Application');
+
+exports.show_goodaps = function(req, res) {
+  res.send('NOT IMPLEMENTED: Table of All Rental Application Data not in Trash');
 };
 
-exports.save_new_ap = function(req, res) {
-  res.send('NOT IMPLEMENTED: Save New (filled in) Application');
+exports.show_trashaps = function(req, res) {
+  res.send('NOT IMPLEMENTED: Table of All Trashed Rental Application Data');
+};
+
+exports.show_goodnames = function(req, res) {
+  res.send('NOT IMPLEMENTED: Listing of All Full Names not in Trash');
+};
+
+exports.show_trashnames = function(req, res) {
+  res.send('NOT IMPLEMENTED: Listing of All Trashed Full Names');
+};
+
+exports.search_allaps = function(req, res) {
+  res.send('NOT IMPLEMENTED: Find All Applications that match pattern: ' + req.params.pattern);
 };
 
 exports.search_goodaps = function(req, res) {
@@ -52,10 +57,14 @@ exports.search_trashaps = function(req, res) {
   res.send('NOT IMPLEMENTED: Find All Trashed Applications that match pattern: ' + req.params.pattern);
 };
 
-exports.search_allaps = function(req, res) {
-  res.send('NOT IMPLEMENTED: Find All Applications that match pattern: ' + req.params.pattern);
-};
-
 exports.search_column = function(req, res) {
   res.send('NOT IMPLEMENTED: Find All Applications that have ' + req.params.search_column + ' that match pattern: ' + req.params.pattern);
+};
+
+exports.search_col-good = function(req, res) {
+  res.send('NOT IMPLEMENTED: Find All Non-Trash Applications that have ' + req.params.search_column + ' that match pattern: ' + req.params.pattern);
+};
+
+exports.search_col-trash = function(req, res) {
+  res.send('NOT IMPLEMENTED: Find All Trashed Applications that have ' + req.params.search_column + ' that match pattern: ' + req.params.pattern);
 };
--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,19 @@ router.get('/:ap_id', rentap_controller.show_ap); //show particular ap for viewi
 router.post('/:ap_id', rentap_controller.save_ap); //if changed and not in trash, can post to save edited ap
 router.post('/:ap_id/discard', rentap_controller.discard_ap); //just puts the rowid in the trash table
 router.post('/trash/:ap_id/delete', rentap_controller.rm_ap); //actually deletes the row from tbl, but only if in trash, getting null if error
-router.post('trash/:ap_id/restore', rentap_controller.restore_ap); //restores by removing rowid from trash table
+router.post('/trash/:ap_id/restore', rentap_controller.restore_ap); //restores by removing rowid from trash table
 
 /* displaying table of matching aps,
  * or just one column from matching aps*/
 router.get('/goodaps', rentap_controller.show_goodaps); //table of aps not in trash
 router.get('/trashaps', rentap_controller.show_trashaps); //table of trashed aps
-router.get('/goodnames', rentap_controller.show_goodaps); //list of names not in trash
+router.get('/goodnames', rentap_controller.show_goodnames); //list of names not in trash
 router.get('/trashnames', rentap_controller.show_trashnames); //list of trashed names
-router.get('/:pattern', rentap_controller.search_allaps); //searches every column for pattern and returns matching rows
-router.get('/goodaps/:pattern', rentap_controller.search_goodaps); //same but exclude rows in trash
-router.get('/trashaps/:pattern', rentap_controller.search_trashaps); //same but only rows in trash
-router.get('/:column/:pattern', rentap_controller.search_column); //search only the specified column for pattern and return matching rows
+router.get('/find/:pattern', rentap_controller.search_allaps); //searches every column for pattern and returns matching rows
+router.get('/find_in_good/:pattern', rentap_controller.search_goodaps); //same but exclude rows in trash
+router.get('/find_in_trash/:pattern', rentap_controller.search_trashaps); //same but only rows in trash
+router.get('/find_in_col/:column/:pattern', rentap_controller.search_column); //search only the specified column for pattern and return matching rows
+router.get('/find_in_col-good/:column/:pattern', rentap_controller.search_col-good); //same but exclude rows in trash
+router.get('/find_in_col-trash/:column/:pattern', rentap_controller.search_col-trash); //same but only rows in trash
 
 module.exports = router;
