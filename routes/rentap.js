322e59da0c20cda5c64adc03d8fcbc717d10e6e3
Colin Keenan <colinnkeenan@gmail.com>
2018-04-19 16:41:53 -0500

    adding routes for headers and starting to figure out form submit buttons. also trying to get the indentation right. doing a lot of coding before I can check anything and so currently definitley not doing anything

--- routes/rentap.js
+++ routes/rentap.js
@@ -20,6 +20,20 @@ router.post('/discard/:ap_id', rentap_controller.discard_ap); //just puts the ro
 router.post('/restore/:ap_id/trash', rentap_controller.restore_ap); //restores by removing rowid from trash table
 router.post('/delete/:ap_id/trash', rentap_controller.rm_ap); //actually deletes the row from tbl, but only if in trash
 
+/* routes for manipulating headers
+ * need ap_id too because want 
+ * to continue showing the same ap
+ * while working on header list */
+router.post('/show/:ap_id/header', rentap_controller.save_new_header); //if not empty, save new header
+router.post('/show/:ap_id/header/:header_id', rentap_controller.save_header_edited); //if actually changed, save changes
+router.post('/show/:ap_id/header/:header_id/delete', rentap_controller.rm_header); //actually deletes the row from headers without verifying
+
+/* guess I need all the same header stuff
+ * when working on a new ap too */
+router.post('/header', rentap_controller.save_new_header); //if not empty, save new header
+router.post('/header/:header_id', rentap_controller.save_header_edited); //if actually changed, save changes
+router.post('/header/:header_id/delete', rentap_controller.rm_header); //actually deletes the row from headers without verifying
+
 /* routes for displaying a table of matching aps,
  * or just one column from matching aps*/
 router.get('/goodaps', rentap_controller.show_goodaps); //table of aps not in trash
