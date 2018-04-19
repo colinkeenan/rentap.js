a11c7a118172aa1277ca9ee3a7bcec52ddd5d631
Colin Keenan <colinnkeenan@gmail.com>
2018-04-18 22:18:34 -0500

    just doing some commenting about how some things work that I was confused about and about what I need to do with rentap.pug

--- routes/rentap.js
+++ routes/rentap.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 var rentap_controller = require('../controllers/rentapController.js');
 
-/* manipulating 
- * individual ap */
+/* All the routes defined below are relative to /rentap because 
+ * in app.js, there's a use /rentap as the router defined in
+ * this file
+ * i.e.  app.use('/rentap', rentapRouter); where var rentapRouter = require('./routes/rentap');
+ */
+
+/* routes for manipulating 
+ * an individual ap */
 router.get('/', rentap_controller.show_new_ap); //home page is just blank ap
 router.get('/show/:ap_id', rentap_controller.show_ap); //show particular ap for viewing/editing/discarding (or deleting/restoring if in trash)
 
@@ -14,7 +20,7 @@ router.post('/discard/:ap_id', rentap_controller.discard_ap); //just puts the ro
 router.post('/restore/:ap_id/trash', rentap_controller.restore_ap); //restores by removing rowid from trash table
 router.post('/delete/:ap_id/trash', rentap_controller.rm_ap); //actually deletes the row from tbl, but only if in trash
 
-/* displaying table of matching aps,
+/* routes for displaying a table of matching aps,
  * or just one column from matching aps*/
 router.get('/goodaps', rentap_controller.show_goodaps); //table of aps not in trash
 router.get('/trashaps', rentap_controller.show_trashaps); //table of trashed aps
